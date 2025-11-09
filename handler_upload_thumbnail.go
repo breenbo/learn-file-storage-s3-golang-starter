912396ecd4a1f344b9d78b05b43a46b9ccd3ecff
Bruno Berrehuel <bruno@homepad.ch>
2025-11-09 18:36:44 +0100

    feat(thumbnails): save b64 in sqlite

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,49 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// max memory to 10 Mo
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form for thumbnail", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get thumbnail file", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-type")
+	image, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read thumbnail file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to upload thumbnail for this video", err)
+		return
+	}
+
+	// update the video metadata
+	b64Image := base64.StdEncoding.EncodeToString(image)
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, b64Image)
+	video.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		// remove the thumb from the map
+		respondWithError(w, http.StatusInternalServerError, "Unable to update the video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
